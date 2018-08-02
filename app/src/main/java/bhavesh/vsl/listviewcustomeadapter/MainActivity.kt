09f71d1ae96b35db0abaecd23eb235e0049b528a
Bhavesh Patel <bhaveshiima@gmail.com>
2018-08-02 17:24:41 +0530

    List view Images of WhatsApp Image folder

--- app/src/main/java/bhavesh/vsl/listviewcustomeadapter/MainActivity.kt
+++ app/src/main/java/bhavesh/vsl/listviewcustomeadapter/MainActivity.kt
@@ -1,12 +1,81 @@
 package bhavesh.vsl.listviewcustomeadapter
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var status = ContextCompat.checkSelfPermission(this ,Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if(status == PackageManager.PERMISSION_GRANTED){
+            // Call ReadFiles function
+            readFiles()
+        }else{
+             //REquest to user to grant permission
+            ActivityCompat.requestPermissions(this  ,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 0)
+
+        }
+
     }
+
+    // to check user grant or not permission  [ START ]
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            readFiles()
+        }else{
+            Toast.makeText(this,
+                    "U can't process for App",
+                    Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    // to check user grant or not permission  [ END ]
+
+
+    // Readfile [ START ]
+    fun readFiles(){
+        //specify the path
+        var path = "/storage/emulated/0/WhatsApp/Media/WhatsApp Images/"
+
+        //convert string obj into file object
+        var file = File(path)
+
+        // now check path
+        if(!file.exists()){
+            var path = "/storage/sdcard0/WhatsApp/Media/WhatsApp Images/"
+            var file = File(path)
+        }
+
+        // declare the array & store data into files arrary
+        var files:Array<String> = file.list()
+
+        // set adapter
+        var myadapter = ArrayAdapter<String>(this ,
+                android.R.layout.simple_list_item_single_choice,files)
+
+        // set adpter to list view 
+        lview.adapter = myadapter
+
+
+
+
+
+    }
+    // Readfile [ END ]
+
+
 }
